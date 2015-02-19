3c3d89895d3cf3ca3e5f34b873bc9e69e6020757
Cedric <cliengme@gmail.com>
2015-02-19 14:34:01 +0100

    IssueType Model + Controller created

--- app/controllers/issueType.js
+++ app/controllers/issueType.js
@@ -1,4 +1,6 @@
-var express = require('express'),
+var 
+_ = require('underscore'),
+express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
   IssueType = mongoose.model('IssueType');
@@ -7,89 +9,69 @@ module.exports = function (app) {
   app.use('/api/issuetypes', router);
 };
 
+
+function convertMongoIssueType(issueType) {
+	//return user.toObject({ transform: true })
+	return {
+		id: issueType.id,
+		firstname: issueType.shortname,
+		description: issueType.description
+	};
+}
 // on routes that end in /issuetypes
 // ----------------------------------------------------
 router.route('/')
     
     // get all the issuetypes (accessed at GET http://localhost:3000/api/issuetypes)
-    .get(function(req, res) {
-        IssueType.find(function(err, issuetypes) {
-            if (err)
-                res.send(err);
-
-            res.json(issuetypes);
-        });
-    })
+	.get(function(req, res, next) {
+		IssueType.find(function (err, issueTypes) {
+		  if (err) return next(err);
+		  res.json(_.map(issueTypes, function(issueType) {
+				return convertMongoIssueType(issueType);
+			}));
+		});
+	})
 
     // create a issuetype (accessed at POST http://localhost:3000/api/issuetype)
-    .post(function(req, res) {
-        
-        var issuetype = new IssueType();      // create a new instance of the IssueType model
-        issuetype.shortname = req.body.shortname;  // set the issuetypes name (comes from the request)
-        issuetype.description = req.body.description;  // set the issuetypes name (comes from the request)
+	.post(function (req, res, next) {
+		var issueType = new IssueType({
+			shortname: req.body.shortname,
+			description: req.body.description
+		});
 
-
-        // save the issuetype and check for errors
-        issuetype.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Issuetype created!' });
-        });
-        
-    });
+		issueType.save(function(err, issueTypeSaved) {
+			res.status(201).json(convertMongoIssueType(issueTypeSaved));
+		});
+	});
 
 // on routes that end in /issuetypes/:issuetype_id
 // ----------------------------------------------------
-router.route('/:issuetype_id')
+router.route('/:id')
 
     // get the issuetype with that id (accessed at GET http://localhost:3000/api/issuetypes/:issuetype_id)
-    .get(function(req, res) {
-        IssueType.findById(req.params.issuetype_id, function(err, issuetype) {
-            if (err)
-                res.send(err);
-            res.json(issuetype);
-        });
-    })
+	.get(function(req, res, next) {
+		IssueType.findById(req.params.id, function(err, issueType) {
+			res.json(convertMongoIssueType(issueType));
+		});
+	})
 
     // update the issuetype with this id (accessed at PUT http://localhost:3000/api/issuetypes/:issuetype_id)
-    .put(function(req, res) {
+	.put(function(req, res, next) {
+		IssueType.findById(req.params.id, function(err, issueType) {
+			issueType.shortname = req.body.shortname;
+			issueType.description = req.body.description;
 
-        // use our issuetype model to find the issuetype we want
-        IssueType.findById(req.params.issuetype_id, function(err, issuetype) {
 
-            if (err)
-                res.send(err);
-
-            //var issuetype = new IssueType();      // create a new instance of the IssueType model
-            issuetype.shortname = req.body.shortname;  // update the issuetypes info
-            issuetype.description = req.body.description;  // update the issuetypes info
-
-            // save the issuetype
-            issuetype.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'Issuetype updated!' });
-            });
-
-        });
-    })
+			issueType.save(function(err, issueTypeSaved) {
+				res.json(convertMongoIssueType(issueTypeSaved));
+			});
+		});
+	})
 
     // delete the issuetype with this id (accessed at DELETE http://localhost:3000/api/issuetypes/:issuetype_id)
-    .delete(function(req, res) {
-        IssueType.remove({
-            _id: req.params.issuetype_id
-        }, function(err, issuetype) {
-            if (err)
-                res.send(err);
+	.delete(function(req, res, next) {
+		IssueType.findByIdAndRemove(req.params.id, function(err) {
+			res.status(204).end();
+		});
+	});
 
-            res.json({ message: 'Successfully deleted' });
-        });
-    });
-/*
-// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
-router.get('/api', function(req, res) {
-    res.json({ message: 'Les routes fonctionnent' });   
-});
-*/
