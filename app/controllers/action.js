a6f2bf03817989e177dd90713f38e60856203db5
Manu Bezençon <Groupe GoF>
2015-02-19 16:43:11 +0100

    Ressource actions
    Création du contrôleur
    —> GET (all)

---
+++ app/controllers/action.js
@@ -0,0 +1,35 @@
+var 
+_ = require('underscore'),
+express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  Action = mongoose.model('Action');
+
+module.exports = function (app) {
+  app.use('/api/actions', router);
+};
+
+
+function convertMongoAction(action) {
+	//return user.toObject({ transform: true })
+	return {
+		id: action.id,
+		type: action.type,
+		content: action.content,
+		creatingDate: action.creatingDate
+	};
+}
+// on routes that end in /contents
+// ----------------------------------------------------
+router.route('/')
+    
+    // get all the actions (accessed at GET http://localhost:3000/api/actions)
+	.get(function(req, res, next) {
+		Action.find(function (err, actions) {
+		  if (err) return next(err);
+		  res.json(_.map(actions, function(action) {
+				return convertMongoAction(action);
+			}));
+		});
+	});
+
