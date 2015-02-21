5d980cc49f3bcd062e2d6a5244299d0ed012b9c3
Cedric <cliengme@gmail.com>
2015-02-21 12:29:43 +0100

    add search query parameter for issuetype

--- app/controllers/issueType.js
+++ app/controllers/issueType.js
@@ -1,86 +1,92 @@
 var
-_ = require('underscore'),
-  express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  IssueType = mongoose.model('IssueType');
+        _ = require('underscore'),
+        express = require('express'),
+        router = express.Router(),
+        mongoose = require('mongoose'),
+        IssueType = mongoose.model('IssueType');
 
-module.exports = function (app) {
-  app.use('/api/v1/issuetypes', router);
+module.exports = function(app) {
+    app.use('/api/v1/issuetypes', router);
 };
 
 
 function convertMongoIssueType(issueType) {
-	//return user.toObject({ transform: true })
-	return {
-		id: issueType.id,
-		shortname: issueType.shortname,
-		description: issueType.description
-	};
+    //return user.toObject({ transform: true })
+    return {
+        id: issueType.id,
+        shortname: issueType.shortname,
+        description: issueType.description
+    };
 }
 // on routes that end in /issuetypes
 // ----------------------------------------------------
 router.route('/')
-    
-    // get all the issuetypes (accessed at GET http://localhost:3000/api/issuetypes)
-	.get(function(req, res, next) {
+
+        // get all the issuetypes (accessed at GET http://localhost:3000/api/issuetypes)
+        .get(function(req, res, next) {
             var by = req.query.by;
             var order = req.query.order;
+            var regex = new RegExp(req.params.search, 'i');
             if (order !== 'asc' && order !== 'desc') {
                 order = 'asc';
-            } 
+            }
             if (by !== 'id' && by !== 'shortname' && by !== 'description') {
                 by = 'shortname';
             }
 
-		IssueType.find().sort([[by, order]]).exec(function (err, issueTypes) {
-		  if (err) return next(err);
-		  res.json(_.map(issueTypes, function(issueType) {
-				return convertMongoIssueType(issueType);
-			}));
-		});
-	})
+            IssueType.find()
+                    .or([{'shortname': regex}, {'description': regex}, {'id': regex}])
+                    .sort([[by, order]])
+                    .skip().limit(5)
+                    .exec(function(err, issueTypes) {
+                        if (err)
+                            return next(err);
+                        res.json(_.map(issueTypes, function(issueType) {
+                            return convertMongoIssueType(issueType);
+                        }));
+                    });
+        })
 
-    // create a issuetype (accessed at POST http://localhost:3000/api/issuetype)
-	.post(function (req, res, next) {
-		var issueType = new IssueType({
-			shortname: req.body.shortname,
-			description: req.body.description
-		});
+        // create a issuetype (accessed at POST http://localhost:3000/api/issuetype)
+        .post(function(req, res, next) {
+            var issueType = new IssueType({
+                shortname: req.body.shortname,
+                description: req.body.description
+            });
 
-		issueType.save(function(err, issueTypeSaved) {
-			res.status(201).json(convertMongoIssueType(issueTypeSaved));
-		});
-	});
+            issueType.save(function(err, issueTypeSaved) {
+                res.status(201).json(convertMongoIssueType(issueTypeSaved));
+            });
+        });
 
 // on routes that end in /issuetypes/:issuetype_id
 // ----------------------------------------------------
 router.route('/:id')
 
-    // get the issuetype with that id (accessed at GET http://localhost:3000/api/issuetypes/:issuetype_id)
-	.get(function(req, res, next) {
-		IssueType.findById(req.params.id, function(err, issueType) {
-			res.json(convertMongoIssueType(issueType));
-		});
-	})
+        // get the issuetype with that id (accessed at GET http://localhost:3000/api/issuetypes/:issuetype_id)
+        .get(function(req, res, next) {
+            IssueType.findById(req.params.id, function(err, issueType) {
+                res.json(convertMongoIssueType(issueType));
+            });
+        })
 
-    // update the issuetype with this id (accessed at PUT http://localhost:3000/api/issuetypes/:issuetype_id)
-	.put(function(req, res, next) {
-		IssueType.findById(req.params.id, function(err, issueType) {
-			issueType.shortname = req.body.shortname;
-			issueType.description = req.body.description;
+        // update the issuetype with this id (accessed at PUT http://localhost:3000/api/issuetypes/:issuetype_id)
+        .put(function(req, res, next) {
+            IssueType.findById(req.params.id, function(err, issueType) {
+                issueType.shortname = req.body.shortname;
+                issueType.description = req.body.description;
 
 
-			issueType.save(function(err, issueTypeSaved) {
-				res.json(convertMongoIssueType(issueTypeSaved));
-			});
-		});
-	})
+                issueType.save(function(err, issueTypeSaved) {
+                    res.json(convertMongoIssueType(issueTypeSaved));
+                });
+            });
+        })
 
-    // delete the issuetype with this id (accessed at DELETE http://localhost:3000/api/issuetypes/:issuetype_id)
-	.delete(function(req, res, next) {
-		IssueType.findByIdAndRemove(req.params.id, function(err) {
-			res.status(204).end();
-		});
-	});
+        // delete the issuetype with this id (accessed at DELETE http://localhost:3000/api/issuetypes/:issuetype_id)
+        .delete(function(req, res, next) {
+            IssueType.findByIdAndRemove(req.params.id, function(err) {
+                res.status(204).end();
+            });
+        });
 
